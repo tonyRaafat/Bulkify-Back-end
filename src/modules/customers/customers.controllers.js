db0e415745ba2d47f45f3710120542a3476c99c3
Anton Raafat <138820743+tonyRaafat@users.noreply.github.com>
2025-03-02 09:26:51 +0200

    fix: replace localhost with base url

--- src/modules/customers/customers.controllers.js
+++ src/modules/customers/customers.controllers.js
@@ -15,6 +15,9 @@ const COORDINATE_LIMITS = {
   MIN_LATITUDE: -90,
   MAX_LATITUDE: 90,
 };
+
+const BASE_URL = "https://bulkify-back-end.vercel.app";
+
 /**
  * Customer registration controller
  */
@@ -126,11 +129,11 @@ export const register = async (req, res, next) => {
     const token = jwt.sign({ email }, process.env.JWT_SECRET, {
       expiresIn: "3m",
     });
-    const verifyLink = `http://localhost:3000/api/v1/customers/verifyEmail/${token}`;
+    const verifyLink = `${BASE_URL}/api/v1/customers/verifyEmail/${token}`;
     const rftoken = jwt.sign({ email }, process.env.JWT_SECRET + "refresh", {
       expiresIn: "3m",
     });
-    const resendLink = `http://localhost:3000/api/v1/customers/refreshtoken/${rftoken}`;
+    const resendLink = `${BASE_URL}/api/v1/customers/refreshtoken/${rftoken}`;
 
     // Send verification email
     await sendEmail(email, "Verify Your Email", {
@@ -179,7 +182,7 @@ export const verifyEmail = async (req, res, next) => {
             expiresIn: "3m",
           }
         );
-        const rflink = `http://localhost:3000/api/v1/customers/refreshtoken/${rftoken}`;
+        const rflink = `${BASE_URL}/api/v1/customers/refreshtoken/${rftoken}`;
 
         throw throwError({
           message: "Verification link has expired",
@@ -226,7 +229,7 @@ export const refreshToken = async (req, res, next) => {
     const token = jwt.sign({ email: decoded.email }, process.env.JWT_SECRET, {
       expiresIn: "3m",
     });
-    const link = `http://localhost:3000/api/v1/customers/verifyEmail/${token}`;
+    const link = `${BASE_URL}/api/v1/customers/verifyEmail/${token}`;
 
     await sendEmail(decoded.email, "Verify Email", {
       text: `Verify your email: ${link}`,
@@ -299,11 +302,11 @@ export const login = async (req, res, next) => {
       const token = jwt.sign({ email }, process.env.JWT_SECRET, {
         expiresIn: "3m",
       });
-      const verifyLink = `http://localhost:3000/api/v1/customers/verifyEmail/${token}`;
+      const verifyLink = `${BASE_URL}/api/v1/customers/verifyEmail/${token}`;
       const rftoken = jwt.sign({ email }, process.env.JWT_SECRET + "refresh", {
         expiresIn: "3m",
       });
-      const resendLink = `http://localhost:3000/api/v1/customers/refreshtoken/${rftoken}`;
+      const resendLink = `${BASE_URL}/api/v1/customers/refreshtoken/${rftoken}`;
 
       // Resend verification email
       await sendEmail(email, "Verify Your Email", {
