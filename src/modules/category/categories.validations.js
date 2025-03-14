ac1d7e6d8aca773fbdc6f72c669edbce580e3028
Anton Raafat <138820743+tonyRaafat@users.noreply.github.com>
2025-03-14 23:49:18 +0200

    fix: category and product validations

--- src/modules/category/categories.validations.js
+++ src/modules/category/categories.validations.js
@@ -1,16 +1,19 @@
 import joi from "joi";
 import { generalField } from "../../utils/generalFields.js";
+import {
+  nameRegex,
+} from "../../constants/constants.js";
 
 export const createCategorySchema = {
   body: joi.object({
-    name: joi.string().min(3).max(25).required(),
+    name: joi.string().min(3).max(25).pattern(nameRegex).required(),
   }),
   headers: generalField.headers,
 };
 
 export const updateCategorySchema = {
   body: joi.object({
-    name: joi.string().min(3).max(25).required(),
+    name: joi.string().min(3).max(25).pattern(nameRegex).required(),
   }),
   params: joi.object({
     id: generalField.id,
