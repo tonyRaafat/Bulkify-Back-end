05f9ac6f00ce652b87d9cbbe6c2d0525a0185ff0
Anton Raafat <138820743+tonyRaafat@users.noreply.github.com>
2025-04-23 00:04:32 +0200

    Update apiFeatuers.js

--- src/utils/apiFeatuers.js
+++ src/utils/apiFeatuers.js
@@ -7,7 +7,7 @@ export class ApiFeatures {
     pagination() {
       let page = this.queryString.page * 1 || 1;
       if (page < 1) page = 1;
-      let limit = 5;
+      let limit = this.queryString.limit || 5;
       let skip = (page - 1) * limit;
       this.query.find().skip(skip).limit(limit);
       this.page = page
