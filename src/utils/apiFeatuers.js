3988ab6b3e863c0f407952cbe0196677a1c12087
MinaHany <hhmina00@gmail.com>
2025-04-29 06:37:16 +0300

    Update: apiFeatures

--- src/utils/apiFeatuers.js
+++ src/utils/apiFeatuers.js
@@ -1,58 +1,55 @@
 export class ApiFeatures {
-    constructor(query, queryString) {
-      this.query = query;
-      this.queryString = queryString;
-    }
-  
-    
-      pagination() {
-  let page = this.queryString.page * 1 || 1;
-  if (page < 1) page = 1;
-  let limit = this.queryString.limit * 1 || 5; // Ensure limit is a number
-  let skip = (page - 1) * limit;
-  this.query = this.query.skip(skip).limit(limit); // Update the query chain correctly
-  this.page = page;
-  return this;
-}
-    
-  
-    filter() {
-      let excludeQuery = ["page", "sort", "search", "select"];
-      let filterQuery = { ...this.queryString };
-      excludeQuery.forEach((e) => delete filterQuery[e]);
-      filterQuery = JSON.parse(
-        JSON.stringify(filterQuery).replace(
-          /(gt|lt|gte|lte|eq)/g,
-          (match) => `$${match}`
-        )
-      );
-      this.query.find(filterQuery);
-      return this;
-    }
-  
-    sort() {
-      if (this.queryString.sort) {
-        this.query.sort(this.queryString.sort.replaceAll(",", " "));
-      }
-      return this;
-    }
-  
-    select() {
-      if (this.queryString.select) {
-        this.query.select(this.queryString.select.replaceAll(",", " "));
-      }
-      return this;
-    }
-  
-    search() {
-      if (this.queryString.search) {
-        this.query.find({
-          $or: [
-            { name: { $regex: this.queryString.search, $options: "i" } },
-            { description: { $regex: this.queryString.search, $options: "i" } },
-          ],
-        });
-      }
-      return this;
-    }
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
   }
+
+  pagination() {
+    let page = this.queryString.page * 1 || 1;
+    if (page < 1) page = 1;
+    let limit = this.queryString.limit * 1 || 5;
+    let skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    this.page = page;
+    return this;
+  }
+
+  filter() {
+    let excludeFields = ["page", "sort", "search", "select"];
+    let filterObj = { ...this.queryString };
+    excludeFields.forEach((field) => delete filterObj[field]);
+
+    let filterStr = JSON.stringify(filterObj);
+    filterStr = filterStr.replace(/\b(gt|gte|lt|lte|eq)\b/g, (match) => `$${match}`);
+    const mongoFilter = JSON.parse(filterStr);
+
+    this.query = this.query.find(mongoFilter); // [تغيير] نحدث this.query مباشرة
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      this.query = this.query.sort(this.queryString.sort.replaceAll(",", " "));
+    }
+    return this;
+  }
+
+  search(fields = []) {
+    if (this.queryString.search && fields.length) {
+      const searchQuery = {
+        $or: fields.map((field) => ({
+          [field]: { $regex: this.queryString.search, $options: "i" }
+        })),
+      };
+      this.query = this.query.find(searchQuery); 
+    }
+    return this;
+  }
+
+  select() {
+    if (this.queryString.select) {
+      this.query = this.query.select(this.queryString.select.replaceAll(",", " "));
+    }
+    return this;
+  }
+}
